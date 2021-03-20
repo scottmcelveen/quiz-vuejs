359a32c5d6aed606351e2f7c096679e5e862b818
Scott McElveen <scott.mcelveen@hotmail.com>
2021-03-20 22:27:30 +0000

    add amplify auth

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <amplify-authenticator>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <div>
+        <router-view/>
+        <amplify-sign-out></amplify-sign-out>
+      </div>
+    </amplify-authenticator>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@aws-amplify/ui-vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports-env';
+Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false
 
