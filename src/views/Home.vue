ef2753d4f179227626692686b389d4c6de4bcbb5
Scott McElveen <scott.mcelveen@hotmail.com>
2021-03-21 20:22:08 +0000

    add a three column layout to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h2>Home.vue</h2>
-    <router-link to="/admin">Go to admin</router-link>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="grey darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
+      <router-link to="/admin">
+        <v-avatar
+          class="hidden-sm-and-down"
+          size="32"
+          tile         
+        >
+        <v-icon large tile>mdi-application-cog</v-icon>
+        </v-avatar>
+      </router-link>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3 pt-lg-0">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
 </script>
